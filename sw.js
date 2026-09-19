5fb49fda833f7f806bd6a5142f287b721c5c8330
MAGICPILLY <pilly_bio@hotmail.com>
2026-09-19 16:13:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('magic-canvas-v1').then((cache) => {
             return cache.addAll([
-                '/MAGIA-IN-CORSIA/',
-                '/MAGIA-IN-CORSIA/index.html',
-                '/MAGIA-IN-CORSIA/style.css',
-                '/MAGIA-IN-CORSIA/app.js',
-                '/MAGIA-IN-CORSIA/manifest.json'
+                '/magia-in-corsia/',
+                '/magia-in-corsia/index.html',
+                '/magia-in-corsia/style.css',
+                '/magia-in-corsia/app.js',
+                '/magia-in-corsia/manifest.json'
             ]);
         })
     );
